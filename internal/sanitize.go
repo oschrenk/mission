1962d113fa2bd5bb057feab6f3570cad7a50074c
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-07-08 09:21:26 -0600

    feat: Remove wikilinks

--- internal/sanitize.go
+++ internal/sanitize.go
@@ -6,9 +6,13 @@ import (
 
 func Sanitize(text string) string {
 	// remove links: [text](link) => text
-	var hrefReg = regexp.MustCompile(`\[(.*?)\][\[\(].*?[\]\)]`)
+	hrefReg := regexp.MustCompile(`\[(.*?)\][\[\(].*?[\]\)]`)
+
+	// remove wikilinks: [[text]] => text
+	wikilinkReg := regexp.MustCompile(`\[\[(.*?)\]\]`)
 
 	text = hrefReg.ReplaceAllString(text, "$1")
+	text = wikilinkReg.ReplaceAllString(text, "$1")
 
 	return text
 }
--- internal/sanitize_test.go
+++ internal/sanitize_test.go
@@ -13,3 +13,13 @@ func TestSanitizeHref(t *testing.T) {
 		t.Fatalf(`%s should equal %s`, sanitized, wants)
 	}
 }
+
+func TestSanitizeWikiLink(t *testing.T) {
+	raw := "Link to [[Wikilink]]"
+	sanitized := Sanitize(raw)
+	wants := "Link to Wikilink"
+
+	if sanitized != wants {
+		t.Fatalf(`%s should equal %s`, sanitized, wants)
+	}
+}
